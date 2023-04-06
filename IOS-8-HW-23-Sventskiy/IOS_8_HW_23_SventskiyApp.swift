a58812d0e04269b0cff2b092fb752eb2a3671319
StanislavSSY <stanislav.mifub@yandex.ru>
2023-04-06 14:21:02 +0700

    Initial Commit

---
+++ IOS-8-HW-23-Sventskiy/IOS_8_HW_23_SventskiyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_8_HW_23_SventskiyApp.swift
+//  IOS-8-HW-23-Sventskiy
+//
+//  Created by Stanislav Stanislav on 06.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct IOS_8_HW_23_SventskiyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
