15754218eee5819fa2942b48318348585ea1f1e0
StanislavSSY <stanislav.mifub@yandex.ru>
2023-04-06 14:54:30 +0700

    Setting project.

---
+++ IOS-8-HW-23-Sventskiy/App/SecondScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SecondScreen.swift
+//  IOS-8-HW-23-Sventskiy
+//
+//  Created by Stanislav Stanislav on 06.04.2023.
+//
+
+import SwiftUI
+
+struct SecondScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondScreen()
+    }
+}
