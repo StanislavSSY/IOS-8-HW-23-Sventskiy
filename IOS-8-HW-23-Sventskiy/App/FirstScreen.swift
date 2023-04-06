46439e68ba6ed8bdd3a85f3f07a27207749bee73
StanislavSSY <stanislav.mifub@yandex.ru>
2023-04-06 17:26:09 +0700

    FirstScreen add elements.

--- IOS-8-HW-23-Sventskiy/App/FirstScreen.swift
+++ IOS-8-HW-23-Sventskiy/App/FirstScreen.swift
@@ -8,19 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedView = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+            NavigationView {
+                
+                
+                VStack {
+                    Text("Ищете свою музыку?")
+                        .bold()
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                        .padding(0)
+                    
+                    Text("Здесь появится купленная Вами в ITunes Store музыка")
+                        .font(.body)
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                    
+                }
+                
+                .navigationTitle("Медиатека")
+                .navigationBarItems(trailing: Button("Править"){ })
+                .foregroundColor(.red)
+                
+                    TabView(selection: $selectedView) {
+                        Text("")
+                            .tabItem {
+                                Image(systemName: "house.fill")
+                                Text("Медиатека")
+                            } .tag(1)
+                        Text("")
+                            .tabItem {
+                                Image(systemName: "dot.radiowaves.left.and.right")
+                                Text("Радио")
+                            } .tag(2)
+                        Text("")
+                            .tabItem {
+                                Image(systemName: "magnifyingglass")
+                                Text("Поиск")
+                            } .tag(3)
+                    }
+                
+                
+                
+                
+                
+                
+                //Image(systemName: "globe")
+                //                    .imageScale(.large)
+                //  .foregroundColor(.accentColor)
+                // Text("Hello, world!")
+                
+                
+                
+                
+                
+            }
         }
-        .padding()
+    
+        
+        
+        struct SecondScreen: View {
+            var body: some View {
+                ZStack {
+                    Color.yellow.edgesIgnoringSafeArea(.all)
+                }
+            }
+            
+            struct ContentView_Previews: PreviewProvider {
+                static var previews: some View {
+                    ContentView()
+                }
+            }
+        }
     }
-}
+    
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
